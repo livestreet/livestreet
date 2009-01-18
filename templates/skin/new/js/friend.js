12395d50e7c39f1953e8f784669ea63a82fb2708
Mzhelskiy Maxim <rus.engine@gmail.com>
2009-01-18 11:22:26 +0000

    fix профиля пользователя

--- templates/skin/new/js/friend.js
+++ templates/skin/new/js/friend.js
@@ -1,26 +1,3 @@
-function ajaxQuestionVote(idTopic,idAnswer) {	
-	JsHttpRequest.query(
-    	DIR_WEB_ROOT+'/include/ajax/questionVote.php',                       
-        { idTopic: idTopic, idAnswer: idAnswer },
-        function(result, errors) {  
-        	if (!result) {
-                msgErrorBox.alert('Error','Please try again later');           
-        	}
-            if (result.bStateError) {
-            	msgErrorBox.alert(result.sMsgTitle,result.sMsg);
-            } else {            	
-            	msgNoticeBox.alert(result.sMsgTitle,result.sMsg);
-            	if ($('topic_question_area_'+idTopic)) {
-            		$('topic_question_area_'+idTopic).set('html',result.sText);
-            	}  
-            }                               
-        },
-        true
-    );	
-}
-
-
-
 function ajaxToggleUserFrend(obj,idUser) {   
 	obj=$(obj);
 	JsHttpRequest.query(
@@ -48,35 +25,4 @@ function ajaxToggleUserFrend(obj,idUser) {
         },
         true
     );
-	
-	
-	
-	
-	
-	
-	
-	/*
-    var req = new JsHttpRequest();    
-    req.onreadystatechange = function() {
-        if (req.readyState == 4) {         
-            //document.getElementById('debug').innerHTML = req.responseText;           
-            if (req.responseJS.bStateError) {
-            	msgErrorBox.alert(req.responseJS.sMsgTitle,req.responseJS.sMsg);
-            } else {            	
-            	msgNoticeBox.alert(req.responseJS.sMsgTitle,req.responseJS.sMsg); 
-            	
-            	if (req.responseJS.bState) {
-            		document.getElementById('user_frend_add').style.display="none";
-            		document.getElementById('user_frend_del').style.display="inline";
-            	} 
-            	if (!req.responseJS.bState) {
-            		document.getElementById('user_frend_add').style.display="inline";
-            		document.getElementById('user_frend_del').style.display="none";
-            	}            	
-            }
-        }
-    }    
-    req.open(null, DIR_WEB_ROOT+'/include/ajax/userFrend.php', true);    
-    req.send( { idUser: idUser, type: type } );
-    */
 }
