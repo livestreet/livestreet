041f462557e91a5e209f82c3d05d575ce52684cd
Alexey Kachayev <kachayev@gmail.com>
2009-08-24 12:31:29 +0000

    В Patch.sql добавлены изменения по системе избранного для писем.

--- patch.sql
+++ patch.sql
@@ -74,3 +74,5 @@ ALTER TABLE  `prefix_favourite` DROP INDEX  `user_id_topic_id`,
 ADD UNIQUE  `user_id_target_id_type` (  `user_id` ,  `target_id` ,  `target_type` );
 ALTER TABLE  `prefix_favourite` ADD INDEX  `target_publish` (  `target_publish` );
 ALTER TABLE `prefix_favourite` ADD CONSTRAINT `prefix_favourite_target_fk` FOREIGN KEY (`user_id`) REFERENCES `prefix_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE  `prefix_favourite` CHANGE  `target_type`  `target_type` ENUM(  'topic',  'comment',  'talk' ) DEFAULT  'topic';
