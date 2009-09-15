b63ce415f44227b61e36f3710e0f3350a5f4251a
Mzhelskiy Maxim <rus.engine@gmail.com>
2009-09-15 19:37:59 +0000

    fix talk bug

--- classes/modules/talk/Talk.class.php
+++ classes/modules/talk/Talk.class.php
@@ -574,6 +574,7 @@ class LsTalk extends Module {
 		 */		
 		foreach ($aTalks as $oTalk) {
 			$aResult=$this->GetTalkUsersByTalkId($oTalk->getId());
+			$aTalkUsers=array();
 			foreach ((array)$aResult as $oTalkUser) {
 				$aTalkUsers[$oTalkUser->getUserId()]=$oTalkUser;
 			}			
