e0a109c13d684a82874ff5dd9fe9dcdb09dde9ac
Mzhelskiy Maxim <rus.engine@gmail.com>
2011-05-03 08:22:13 +0000

    fix developer-jquery

--- templates/skin/developer-jquery/actions/ActionProfile/friend_item.tpl
+++ templates/skin/developer-jquery/actions/ActionProfile/friend_item.tpl
@@ -1,17 +1,17 @@
 {if $oUserFriend and ($oUserFriend->getFriendStatus()==$USER_FRIEND_ACCEPT+$USER_FRIEND_OFFER or $oUserFriend->getFriendStatus()==$USER_FRIEND_ACCEPT+$USER_FRIEND_ACCEPT)}
-	<li id="delete_friend_item"><a href="#"  title="{$aLang.user_friend_del}" onclick="ajaxDeleteUserFriend(this,{$oUserProfile->getId()},'del'); return false;">{$aLang.user_friend_del}</a></li>
+	<li id="delete_friend_item"><a href="#"  title="{$aLang.user_friend_del}" onclick="return ls.user.removeFriend(this,{$oUserProfile->getId()},'del');">{$aLang.user_friend_del}</a></li>
 {elseif $oUserFriend and $oUserFriend->getStatusTo()==$USER_FRIEND_REJECT and $oUserFriend->getStatusFrom()==$USER_FRIEND_OFFER and $oUserFriend->getUserTo()==$oUserCurrent->getId()}
-	<li id="add_friend_item"><a href="#"  title="{$aLang.user_friend_add}" onclick="ajaxAddUserFriend(this,{$oUserProfile->getId()},'accept'); return false;">{$aLang.user_friend_add}</a></li>
+	<li id="add_friend_item"><a href="#"  title="{$aLang.user_friend_add}" onclick="return ls.user.addFriend(this,{$oUserProfile->getId()},'accept');">{$aLang.user_friend_add}</a></li>
 {elseif $oUserFriend and $oUserFriend->getFriendStatus()==$USER_FRIEND_OFFER+$USER_FRIEND_REJECT and $oUserFriend->getUserTo()!=$oUserCurrent->getId()}
 	<li>{$aLang.user_friend_offer_reject}</li>							
 {elseif $oUserFriend and $oUserFriend->getFriendStatus()==$USER_FRIEND_OFFER+$USER_FRIEND_NULL and $oUserFriend->getUserFrom()==$oUserCurrent->getId()}
 	<li>{$aLang.user_friend_offer_send}</li>						
 {elseif $oUserFriend and $oUserFriend->getFriendStatus()==$USER_FRIEND_OFFER+$USER_FRIEND_NULL and $oUserFriend->getUserTo()==$oUserCurrent->getId()}
-	<li id="add_friend_item"><a href="#"  title="{$aLang.user_friend_add}" onclick="ajaxAddUserFriend(this,{$oUserProfile->getId()},'accept'); return false;">{$aLang.user_friend_add}</a></li>
+	<li id="add_friend_item"><a href="#"  title="{$aLang.user_friend_add}" onclick="return ls.user.addFriend(this,{$oUserProfile->getId()},'accept');">{$aLang.user_friend_add}</a></li>
 {elseif !$oUserFriend}	
 	<li id="add_friend_item"><a href="#"  title="{$aLang.user_friend_add}" id="add_friend_show">{$aLang.user_friend_add}</a>						</li>
 
-	<form id="add_friend_form" class="add-friend-form" onsubmit="ajaxAddUserFriend(this,{$oUserProfile->getId()},'add'); return false;">
+	<form id="add_friend_form" class="add-friend-form" onsubmit="return ls.user.addFriend(this,{$oUserProfile->getId()},'add');">
 		<a href="#" class="close jqmClose"></a>
 		
 		<label for="add_friend_text">{$aLang.user_friend_add_text_label}</label><br />
@@ -20,5 +20,5 @@
 		<input type="submit" value="{$aLang.user_friend_add_submit}" />
 	</form>	
 {else}
-	<li id="add_friend_item"><a href="#" title="{$aLang.user_friend_add}" onclick="ajaxAddUserFriend(this,{$oUserProfile->getId()},'link'); return false;">{$aLang.user_friend_add}</a></li>
+	<li id="add_friend_item"><a href="#" title="{$aLang.user_friend_add}" onclick="return ls.user.addFriend(this,{$oUserProfile->getId()},'link');">{$aLang.user_friend_add}</a></li>
 {/if}
--- templates/skin/developer-jquery/js/friend.js
+++ templates/skin/developer-jquery/js/friend.js
@@ -1,42 +1,59 @@
-function ajaxAddUserFriend(obj, idUser, sAction) {
-	if(sAction != 'link' && sAction != 'accept') {
-		sText = $('#add_friend_text').val();
-		$('#add_friend_form').children().each(function(i, item){$(item).attr('disabled','disabled')});
-	} else {
-		sText='';
-	}
-	
-	if(sAction == 'accept') {
-		sPath = aRouter.profile+'ajaxfriendaccept/';
-	} else {
-		sPath = aRouter.profile+'ajaxfriendadd/';
-	}
-	
-	$.post(sPath, { idUser: idUser, userText: sText, security_ls_key: LIVESTREET_SECURITY_KEY }, function(result){
-		if (!result) {
-			$.notifier.error('Error','Please try again later');         
-			$('#add_friend_form').children().each(function(i, item){$(item).removeAttr('disabled')});
-		}
-		if (result.bStateError) {
-			$.notifier.error(null,result.sMsg);
-			$('#add_friend_form').children().each(function(i, item){$(item).removeAttr('disabled')});
-		} else {            	
-			$.notifier.notice(null,result.sMsg);
-			$('#add_friend_form').jqmHide();
-			$('#add_friend_item').remove();
-			$('#profile_actions').prepend($(result.sToggleText));           		
-		}
-	});
-}
+var ls = ls || {};
 
-function ajaxDeleteUserFriend(obj,idUser,sAction) {   
-	$.post(aRouter.profile+'ajaxfrienddelete/', { idUser: idUser,sAction: sAction, security_ls_key: LIVESTREET_SECURITY_KEY }, function(result) {
-		if (result.bStateError) {
-			$.notifier.error(null,result.sMsg);
-		} else {            	
-			$.notifier.notice(null,result.sMsg);
-			$('#delete_friend_item').remove();            		
-			$('#profile_actions').prepend($(result.sToggleText)); 
+/**
+* Управление пользователями
+*/
+ls.user = (function ($) {
+	
+	/**
+	* Добавление в друзья
+	*/
+	this.addFriend = function(obj, idUser, sAction){
+		if(sAction != 'link' && sAction != 'accept') {
+			sText = $('#add_friend_text').val();
+			$('#add_friend_form').children().each(function(i, item){$(item).attr('disabled','disabled')});
+		} else {
+			sText='';
 		}
-	});
-}
+
+		if(sAction == 'accept') {
+			sPath = aRouter.profile+'ajaxfriendaccept/';
+		} else {
+			sPath = aRouter.profile+'ajaxfriendadd/';
+		}
+
+		ls.ajax(sPath, {idUser: idUser, userText: sText}, function(result){
+			$('#add_friend_form').children().each(function(i, item){$(item).removeAttr('disabled')});
+			if (!result) {
+				ls.msg.error('Error','Please try again later');
+			}
+			if (result.bStateError) {
+				ls.msg.error(null,result.sMsg);
+			} else {
+				ls.msg.notice(null,result.sMsg);
+				$('#add_friend_form').jqmHide();
+				$('#add_friend_item').remove();
+				$('#profile_actions').prepend($(result.sToggleText));
+			}
+		});
+		return false;
+	};
+
+	/**
+	* Удаление из друзей
+	*/
+	this.removeFriend = function(obj,idUser,sAction) {
+		ls.ajax(aRouter.profile+'ajaxfrienddelete/', {idUser: idUser,sAction: sAction}, function(result) {
+			if (result.bStateError) {
+				ls.msg.error(null,result.sMsg);
+			} else {
+				ls.msg.notice(null,result.sMsg);
+				$('#delete_friend_item').remove();
+				$('#profile_actions').prepend($(result.sToggleText));
+			}
+		});
+		return false;
+	}
+	
+	return this;
+}).call(ls.user || {},jQuery);
