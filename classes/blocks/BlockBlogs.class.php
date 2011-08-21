def6816a6148ee3e22038b2fb0de096475e92ab8
Mzhelskiy Maxim <rus.engine@gmail.com>
2011-08-21 16:14:13 +0000

    fix blocks

--- classes/blocks/BlockBlogs.class.php
+++ classes/blocks/BlockBlogs.class.php
@@ -23,8 +23,9 @@ class BlockBlogs extends Block {
 	public function Exec() {
 		if ($aResult=$this->Blog_GetBlogsRating(1,Config::Get('block.blogs.row'))) {
 			$aBlogs=$aResult['collection'];
-			$this->Viewer_Assign('aBlogs',$aBlogs);
-			$sTextResult=$this->Viewer_Fetch("block.blogs_top.tpl");
+			$oViewer=$this->Viewer_GetLocalViewer();
+			$oViewer->Assign('aBlogs',$aBlogs);
+			$sTextResult=$oViewer->Fetch("block.blogs_top.tpl");
 			$this->Viewer_Assign('sBlogsTop',$sTextResult);
 		}
 	}
--- classes/blocks/BlockStream.class.php
+++ classes/blocks/BlockStream.class.php
@@ -22,8 +22,9 @@
 class BlockStream extends Block {
 	public function Exec() {
 		if ($aComments=$this->Comment_GetCommentsOnline('topic',Config::Get('block.stream.row'))) {
-			$this->Viewer_Assign('aComments',$aComments);
-			$sTextResult=$this->Viewer_Fetch("block.stream_comment.tpl");
+			$oViewer=$this->Viewer_GetLocalViewer();
+			$oViewer->Assign('aComments',$aComments);
+			$sTextResult=$oViewer->Fetch("block.stream_comment.tpl");
 			$this->Viewer_Assign('sStreamComments',$sTextResult);
 		}
 	}
