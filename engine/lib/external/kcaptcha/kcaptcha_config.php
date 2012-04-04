b5f84f036e16b8babbcee7120f79acd60175cca1
Mzhelskiy Maxim <rus.engine@gmail.com>
2012-04-04 15:10:04 +0400

    Обновление KCaptcha до 2.0

--- engine/lib/external/kcaptcha/kcaptcha_config.php
+++ engine/lib/external/kcaptcha/kcaptcha_config.php
@@ -6,21 +6,28 @@ $alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"; # do not change without chan
 
 # symbols used to draw CAPTCHA
 //$allowed_symbols = "0123456789"; #digits
-$allowed_symbols = "23456789abcdeghkmnpqsuvxyz"; #alphabet without similar symbols (o=0, 1=l, i=j, t=f)
+//$allowed_symbols = "23456789abcdegkmnpqsuvxyz"; #alphabet without similar symbols (o=0, 1=l, i=j, t=f)
+$allowed_symbols = "23456789abcdegikpqsvxyz"; #alphabet without similar symbols (o=0, 1=l, i=j, t=f)
 
 # folder with fonts
 $fontsdir = 'fonts';	
 
 # CAPTCHA string length
-//$length = mt_rand(5,6); # random 5 or 6
+//$length = mt_rand(5,7); # random 5 or 6 or 7
 $length = 3;
 
-# CAPTCHA image size (you do not need to change it, whis parameters is optimal)
+# CAPTCHA image size (you do not need to change it, this parameters is optimal)
 $width = 80;
-$height = 40;
+$height = 60;
 
-# symbol's vertical fluctuation amplitude divided by 2
-$fluctuation_amplitude = 5;
+# symbol's vertical fluctuation amplitude
+$fluctuation_amplitude = 8;
+
+#noise
+//$white_noise_density=0; // no white noise
+$white_noise_density=1/6;
+//$black_noise_density=0; // no black noise
+$black_noise_density=1/30;
 
 # increase safety by prevention of spaces between symbols
 $no_spaces = true;
@@ -32,8 +39,8 @@ $credits = 'www.captcha.ru'; # if empty, HTTP_HOST will be shown
 # CAPTCHA image colors (RGB, 0-255)
 //$foreground_color = array(0, 0, 0);
 //$background_color = array(220, 230, 255);
-$foreground_color = array(mt_rand(0,100), mt_rand(0,100), mt_rand(0,100));
-$background_color = array(mt_rand(200,255), mt_rand(200,255), mt_rand(200,255));
+$foreground_color = array(mt_rand(0,80), mt_rand(0,80), mt_rand(0,80));
+$background_color = array(mt_rand(220,255), mt_rand(220,255), mt_rand(220,255));
 
 # JPEG quality of CAPTCHA image (bigger is better quality, but larger file size)
 $jpeg_quality = 90;
