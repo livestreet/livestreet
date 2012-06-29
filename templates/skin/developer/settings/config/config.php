2c3fab8ca0fde0d1998fc4a2269338cfaff8e87a
Denis Shakhov <denis.shakhov@gmail.com>
2012-06-29 15:56:21 +0700

    Стили для печати

--- templates/skin/developer/settings/config/config.php
+++ templates/skin/developer/settings/config/config.php
@@ -33,6 +33,7 @@ $config['head']['default']['css'] = array(
 	"___path.static.skin___/css/jquery.notifier.css",
 	"___path.static.skin___/css/smoothness/jquery-ui.css",
 	"___path.static.skin___/themes/___view.theme___/style.css",
+	"___path.static.skin___/css/print.css",
 );
 
 
