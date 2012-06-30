8d0cee144642985ed0f42cbe21cf3f1e9831dd0b
Denis Shakhov <denis.shakhov@gmail.com>
2012-06-30 16:22:39 +0700

    Фикс багов

--- templates/skin/developer/js/template.js
+++ templates/skin/developer/js/template.js
@@ -3,6 +3,23 @@ jQuery(document).ready(function($){
 	ls.hook.run('ls_template_init_start',[],window);
 	
 	$('html').removeClass('no-js');
+	
+	// Определение браузера
+	if ($.browser.opera) {
+		$('body').addClass('opera opera' + parseInt($.browser.version));
+	}
+	if ($.browser.mozilla) {
+		$('body').addClass('mozilla mozilla' + parseInt($.browser.version));
+	}
+	if ($.browser.webkit) {
+		$('body').addClass('webkit webkit' + parseInt($.browser.version));
+	}
+	if ($.browser.msie) {
+		$('body').addClass('ie');
+		if (parseInt($.browser.version) > 8) {
+			$('body').addClass('ie' + parseInt($.browser.version));
+		}
+	}
 	 
 	// Всплывающие окна
 	$('#window_login_form').jqm();
