097c712618790fda4f0e5d03be6e25cae20e6cde
Babichev Igor <himic5@yandex.ru>
2012-07-07 23:41:26 +0600

    Update master

--- classes/actions/ActionPeople.class.php
+++ classes/actions/ActionPeople.class.php
@@ -236,14 +236,14 @@ class ActionPeople extends Action {
 		 */
 		$sOrder='user_rating';
 		if (getRequest('order')) {
-			$sOrder=getRequest('order');
+			$sOrder=(string)getRequest('order');
 		}
 		/**
 		 * В каком направлении сортировать
 		 */
 		$sOrderWay='desc';
 		if (getRequest('order_way')) {
-			$sOrderWay=getRequest('order_way');
+			$sOrderWay=(string)getRequest('order_way');
 		}
 		$aFilter=array(
 			'activate' => 1
