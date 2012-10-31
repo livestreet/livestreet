f7b559ef492b771ec9b609b53d0ea5408689bb08
Golfersds <Golfersds@gmail.com>
2012-10-31 12:20:25 +0200

    restored BaseFeaturesContext

---
+++ tests/behat/features/bootstrap/BaseFeatureContext.php
@@ -0,0 +1,69 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\MinkExtension\Context\MinkContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+$sDirRoot = dirname(realpath((dirname(__FILE__)) . "/../../../"));
+set_include_path(get_include_path().PATH_SEPARATOR.$sDirRoot);
+
+require_once("tests/LoadFixtures.php");
+
+/**
+ * LiveStreet custom feature context
+ */
+class FeatureContext extends MinkContext
+{
+
+    protected static $fixturesLoader = null;
+
+    /**
+     * Get fixtures loader
+     * @return LoadFixtures
+     */
+    protected static function getFixturesLoader()
+    {
+        if (is_null(self::$fixturesLoader)) {
+            self::$fixturesLoader = new LoadFixtures();
+        }
+
+        return self::$fixturesLoader;
+    }
+
+    /**
+     * Purge DB and load fixtures before running each test
+     *
+     * @BeforeScenario
+     */
+    public static function prepare($event){
+        $fixturesLoader = self::getFixturesLoader();
+        $fixturesLoader->purgeDB();
+        $fixturesLoader->load();
+    }
+
+    /**
+     * Loading fixture for plugin
+     *
+     * @Given /^I load fixtures for plugin "([^"]*)"$/
+     */
+    public function loadFixturesForPlugin($plugin)
+    {
+        $fixturesLoader = $this->getFixturesLoader();
+        $fixturesLoader->loadPluginFixtures($plugin);
+    }
+
+
+    /**
+     * @Given /^I am activated plugin "([^"]*)"$/
+     */
+    public function ActivatedPlugin($plugin)
+    {
+        $pluginActivation =  new LoadFixtures();
+        $pluginActivation->activationPlugin($plugin);
+    }
+
+}
