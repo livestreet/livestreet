629555cf2a451685f222cf81a8a1d11f064a5be2
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2012-11-23 00:38:56 +0200

    rm sql files from tests. added symlinks for sql files from install

--- tests/travis/mysql_setup.sh
+++ tests/travis/mysql_setup.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get install mysql-server mysql-client
 mysql -u root -e 'CREATE DATABASE social_test;'
-mysql -u root -B social_test < ./tests/fixtures/sql/install_base.sql
+mysql -u root -B social_test < ./tests/fixtures/sql/sql.sql
 mysql -u root -B social_test < ./tests/fixtures/sql/geo_base.sql
