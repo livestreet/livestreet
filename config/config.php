0d2a9b7e66468c2ec1389768d80e2550ff84c580
Mzhelskiy Maxim <rus.engine@gmail.com>
2012-12-18 17:07:24 +0400

    fix #174

--- config/config.php
+++ config/config.php
@@ -82,6 +82,7 @@ $config['path']['smarty']['template'] = '___path.root.server___/templates/skin/_
 $config['path']['smarty']['compiled'] = '___path.root.server___/templates/compiled';
 $config['path']['smarty']['cache']    = '___path.root.server___/templates/cache';
 $config['path']['smarty']['plug']     = '___path.root.engine___/modules/viewer/plugs';
+$config['smarty']['compile_check']    = true; // Проверять или нет файлы шаблона на изменения перед компиляцией, false может значительно увеличить быстродействие, но потребует ручного удаления кеша при изменения шаблона
 /**
  * Настройки плагинов
  */
