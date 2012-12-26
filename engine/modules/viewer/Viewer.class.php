1bb9f13eb75eacfa7311f58f9c2fdd4875d0e842
Mzhelskiy Maxim <rus.engine@gmail.com>
2012-12-26 19:03:59 +0400

    Issue #169

--- engine/modules/viewer/Viewer.class.php
+++ engine/modules/viewer/Viewer.class.php
@@ -216,6 +216,7 @@ class ModuleViewer extends Module {
 		$this->oSmarty->setCompileDir($sCompilePath);
 		$this->oSmarty->setCacheDir(Config::Get('path.smarty.cache'));
 		$this->oSmarty->addPluginsDir(array(Config::Get('path.smarty.plug'),'plugins'));
+		$this->oSmarty->default_template_handler_func=array($this,'SmartyDefaultTemplateHandler');
 		/**
 		 * Получаем настройки JS, CSS файлов
 		 */
@@ -1392,6 +1393,42 @@ class ModuleViewer extends Module {
 		foreach ($this->aMenu as $sContainer=>$sTemplate) {
 			$this->aMenuFetch[$sContainer]=$this->Fetch($sTemplate);
 		}
+	}
+	/**
+	 * Обработка поиска файла шаблона, если его не смог найти шаблонизатор Smarty
+	 *
+	 * @param string $sType	Тип шаблона/ресурса
+	 * @param string $sName	Имя шаблона - имя файла
+	 * @param string $sContent	Возврат содержания шаблона при return true;
+	 * @param int $iTimestamp	Возврат даты модификации шаблона при return true;
+	 * @param Smarty $oSmarty	Объект Smarty
+	 * @return string|bool
+	 */
+	public function SmartyDefaultTemplateHandler($sType,$sName,&$sContent,&$iTimestamp,$oSmarty) {
+		/**
+		 * Название шаблона может содержать, как полный путь до файла шаблона, так и относительный любого из каталога в $oSmarty->getTemplateDir()
+		 * По дефолту каталоги такие: /templates/skin/[name]/ и /plugins/
+		 */
+		/**
+		 * Задача: если это файл плагина для текущего шаблона, то смотрим этот же файл шаблона плагина в /default/
+		 */
+		if (Config::Get('view.skin')!='default') {
+			// /root/plugins/[plugin name]/templates/skin/[skin name]/dir/test.tpl
+			if (preg_match('@^'.preg_quote(Config::Get('path.root.server')).'/plugins/([\w\-_]+)/templates/skin/'.preg_quote(Config::Get('view.skin')).'/@i',$sName,$aMatch)) {
+				$sFile=str_replace($aMatch[0],Config::Get('path.root.server').'/plugins/'.$aMatch[1].'/templates/skin/default/',$sName);
+				if ($this->TemplateExists($sFile)) {
+					return $sFile;
+				}
+			}
+			// [plugin name]/templates/skin/[skin name]/dir/test.tpl
+			if (preg_match('@^([\w\-_]+)/templates/skin/'.preg_quote(Config::Get('view.skin')).'/@i',$sName,$aMatch)) {
+				$sFile=Config::Get('path.root.server').'/plugins/'.str_replace($aMatch[0],$aMatch[1].'/templates/skin/default/',$sName);
+				if ($this->TemplateExists($sFile)) {
+					return $sFile;
+				}
+			}
+		}
+		return false;
 	}
 	/**
 	 * Загружаем переменные в шаблон при завершении модуля
