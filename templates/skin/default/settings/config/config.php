7ac963c1614c9f6721c18ae2634acd1913fcdd50
Mzhelskiy Maxim <rus.engine@gmail.com>
2013-01-08 16:54:07 +0400

    fix config

--- templates/skin/default/settings/config/config.php
+++ templates/skin/default/settings/config/config.php
@@ -6,7 +6,11 @@ $config['head']['default']['js'] = Config::Get('head.default.js');
 $config['head']['default']['js'][] = '___path.static.skin___/js/template.js';
 
 $config['head']['default']['css'] = array(
-	"___path.static.skin___/css/reset.css",
+	// Framework styles
+	"___path.root.server___/templates/framework/css/reset.css",
+	"___path.root.server___/templates/framework/css/helpers.css",
+	"___path.root.server___/templates/framework/css/text.css",
+	// Template styles
 	"___path.static.skin___/css/main.css",
 	"___path.static.skin___/css/modals.css",
 );
