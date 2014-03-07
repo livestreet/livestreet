9725767e1781b35f1afbf9b812891c85b0fbeaf5
Marina Goritskaia <goritskaia@yandex.ru>
2014-03-07 03:14:36 +0300

    Update en.php

--- application/frontend/i18n/en.php
+++ application/frontend/i18n/en.php
@@ -684,7 +684,7 @@ return array(
 	'settings_tuning_submit' => 'save settings',
 	'settings_tuning_submit_ok' => 'Settings saved',
 	'settings_account' => 'Account settings',
-	blog_closedblog_closed'settings_account_password' => 'Password',
+	'settings_account_password' => 'Password',
 	'settings_account_password_notice' => 'Leave the fields blank if you don\'t want to change your password',
 	'settings_account_submit' => 'Save changes',
 	'settings_account_submit_ok' => 'Account saved',
