ea37e4b8f8a364b444684daf30afdd742dd8d6bf
Mzhelskiy Maxim <rus.engine@gmail.com>
2015-01-29 16:59:21 +0700

    fix template

--- application/frontend/skin/developer/settings/config/config.php
+++ application/frontend/skin/developer/settings/config/config.php
@@ -21,14 +21,14 @@ $config['view']['grid']['fixed_width'] = '1000px';
 $config['view']['layout_show_banner'] = true;
 
 // Подключение скриптов шаблона
-$config['head']['default']['js'] = array_merge(Config::Get('head.default.js'), array(
+$config['head']['template']['js'] = array(
 	'___path.skin.assets.web___/js/init.js'
-));
+);
 
 // Подключение стилей шаблона
-$config['head']['default']['css'] = array_merge(Config::Get('head.default.css'), array(
+$config['head']['template']['css'] = array(
 	"___path.skin.assets.web___/css/layout.css",
 	"___path.skin.assets.web___/css/print.css"
-));
+);
 
 return $config;
