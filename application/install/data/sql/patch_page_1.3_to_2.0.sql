f0077cadca495ef7788c242a69d3dafc9d392ba9
Mzhelskiy Maxim <rus.engine@gmail.com>
2016-10-19 19:11:09 +0700

    fixes

--- application/install/data/sql/patch_page_1.3_to_2.0.sql
+++ application/install/data/sql/patch_page_1.3_to_2.0.sql
@@ -12,3 +12,5 @@ ALTER TABLE `prefix_page` CHANGE `page_active` `active` TINYINT( 1 ) UNSIGNED NO
 ALTER TABLE `prefix_page` CHANGE `page_main` `main` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT '0';
 ALTER TABLE `prefix_page` CHANGE `page_sort` `sort` INT( 11 ) NOT NULL ;
 ALTER TABLE `prefix_page` CHANGE `page_auto_br` `auto_br` TINYINT( 1 ) NOT NULL DEFAULT '1';
+ALTER TABLE `prefix_page` ADD `text_source` TEXT NULL DEFAULT NULL AFTER `text`;
+UPDATE `prefix_page` SET text_source = text;
