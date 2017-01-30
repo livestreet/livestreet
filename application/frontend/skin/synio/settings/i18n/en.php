80442c621af143c141f1eadc11408730410c6ea6
Xor <k@shvebs.com>
2017-01-30 12:37:49 -0800

    Pre-alpha of English translation

---
+++ application/frontend/skin/synio/settings/i18n/en.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'synio' => [
+        'guest' => 'Guest',
+        'site_pages' => 'Pages',
+        'drafts' => '%%count%% draft;%%count%% drafts;%%count%% drafts',
+        'user_about_empty' => 'No data yet...',
+        'all_blogs' => 'All blogs'
+    ],
+    'activity' => [
+        'block_recent' => [
+            'all' => 'All activities'
+        ]
+    ],
+    'user' => [
+        'search' => [
+            'title' => 'Search'
+        ]
+    ]
+];
