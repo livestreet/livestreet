25d155c79af82367a67f17f8b6ba6a95b149fdf9
Олег <end-fin@mail.ru>
2018-05-15 14:50:59 +0600

    Update TopicTag.entity.class.php
    Orm убрано

--- application/classes/modules/topic/entity/TopicTag.entity.class.php
+++ application/classes/modules/topic/entity/TopicTag.entity.class.php
@@ -25,7 +25,7 @@
  * @package application.modules.topic
  * @since 1.0
  */
-class ModuleTopic_EntityTopicTag extends EntityORM
+class ModuleTopic_EntityTopicTag extends Entity
 {
     /**
      * Возвращает ID тега
